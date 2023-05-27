ec19ae47ff32b8ee144d56fdafd944db81c3f5fb
dDmioko <aalexn64@gmail.com>
2023-05-27 21:30:48 +0700

    Add game to wpf

--- MinesweeperWPF/MainWindow.xaml.cs
+++ MinesweeperWPF/MainWindow.xaml.cs
@@ -1,28 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MinesweeperWPF.Lib;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace MinesweeperWPF
+namespace MinesweeperWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private const int BoardSize = 10; // Размер игрового поля
+    private const int MineCount = 15; // Количество мин
+
+    private Game _game = new(BoardSize, MineCount);
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        GenerateBoard();
+    }
+
+    private void GenerateBoard()
+    {
+
+        BoardGrid.Rows = BoardSize;
+        BoardGrid.Columns = BoardSize;
+
+        foreach (var cell in _game.Board)
         {
-            InitializeComponent();
+            cell.Click += Cell_MouseLeftButtonDown;
+            BoardGrid.Children.Add(cell);
         }
+    }
+
+    private void Cell_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+    {
+        var cell = (Cell)sender;
+        if (cell.HasMine)
+        {
+            // Игра проиграна
+            _game.ShowAllMines();
+            MessageBox.Show("Вы проиграли!", "Поражение", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            ResetGame();
+        }
+        else
+        {
+            _game.OpenCell(cell);
+            if (!_game.IsGameEnded) return;
+
+            // Игра выиграна
+            MessageBox.Show("Поздравляю, вы выиграли!", "Победа", MessageBoxButton.OK, MessageBoxImage.Information);
+            ResetGame();
+        }
+    }
+
+    private void ResetGame()
+    {
+        BoardGrid.Children.Clear();
+        _game = new Game(BoardSize, MineCount);
+        GenerateBoard();
     }
 }
