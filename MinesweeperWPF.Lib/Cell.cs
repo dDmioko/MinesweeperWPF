c6b0aee2bbe743f6077b2abead37a842605ff3d5
dDmioko <aalexn64@gmail.com>
2023-05-27 20:16:34 +0700

    Add game project

---
+++ MinesweeperWPF.Lib/Cell.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MinesweeperWPF.Lib;
+
+public sealed class Cell : Button
+{
+    public int Row { get; }
+    public int Column { get; }
+    public bool HasMine { get; set; } = false;
+    public bool HasFlag { get; set; } = false;
+
+    private bool isOpened = false;
+    public bool IsOpened
+    {
+        get => isOpened;
+        set
+        {
+            isOpened = value;
+
+            // Обновляем цвет и стиль ячейки
+            Content = HasMine ? "*" : null;
+            Foreground = HasMine ? Brushes.Red : Brushes.LightGray;
+            FontWeight = FontWeights.Bold;
+
+            // При попадании на мину игра заканчивается
+            if (!HasMine) IsEnabled = false;
+        }
+    }
+
+    public Cell(int row, int column)
+    {
+        Row = row;
+        Column = column;
+
+        //// Высота/ширина кнопки
+        //Width = 40;
+        //Height = 40;
+
+        // Выравнивание содержимого по центру
+        HorizontalContentAlignment = HorizontalAlignment.Center;
+        VerticalContentAlignment = VerticalAlignment.Center;
+
+        // Размер шрифта
+        FontSize = 20;
+
+        // Толщина границ кнопки
+        BorderThickness = new Thickness(1);
+        // Цвет границ кнопки
+        BorderBrush = Brushes.Black;
+        // Цвет кнопки
+        Background = Brushes.LightBlue;
+    }
+}
---
+++ MinesweeperWPF.Lib/Game.cs
@@ -0,0 +1,108 @@
+﻿namespace MinesweeperWPF.Lib;
+
+/// <summary>
+/// Класс-релизация игры Сапёр
+/// </summary>
+public class Game
+{
+    /// <summary>
+    /// Размер игрового поля
+    /// </summary>
+    public int BoardSize { get; }
+
+    /// <summary>
+    /// Количество мин
+    /// </summary>
+    public int MineCount { get; }
+
+    private readonly Cell[,] board;
+    /// <summary>
+    /// Массив ячеек игрового поля
+    /// </summary>
+    public Cell[,] Board => board;
+
+    private int remainingCells; // Количество оставшихся неоткрытых ячеек
+
+    public Game(int boardSize, int mineCount)
+    {
+        BoardSize = boardSize;
+        MineCount = mineCount;
+
+        board = new Cell[BoardSize, BoardSize];
+        remainingCells = BoardSize * BoardSize;
+
+        GenerateBoard();
+    }
+
+    private void GenerateBoard()
+    {
+        // Создаем сетку ячеек
+        for (int row = 0; row < BoardSize; row++)
+        {
+            for (int col = 0; col < BoardSize; col++)
+            {
+                var cell = new Cell(row, col);
+                board[row, col] = cell;
+            }
+        }
+
+        // Размещаем мины случайным образом
+        var random = new Random();
+        int minesPlaced = 0;
+        while (minesPlaced < MineCount)
+        {
+            int row = random.Next(BoardSize);
+            int col = random.Next(BoardSize);
+            var cell = board[row, col];
+            if (!cell.HasMine)
+            {
+                cell.HasMine = true;
+                minesPlaced++;
+            }
+        }
+    }
+
+    public void OpenCell(Cell cell)
+    {
+        if (cell.IsOpened || cell.HasFlag) return;
+
+        cell.IsOpened = true;
+        remainingCells--;
+
+        // Подсчитываем количество мин вокруг ячейки
+        int mineCount = 0;
+        DoActionAroundCell(cell, (_, _) => mineCount++);
+
+        // Обновляем текст ячейки
+        if (mineCount > 0)
+        {
+            cell.Content = mineCount.ToString();
+            return;
+        }
+
+        // Если вокруг ячейки нет мин, открываем соседние ячейки рекурсивно
+        DoActionAroundCell(cell, (int i, int j) => OpenCell(board[i, j]));
+    }
+
+    public void ShowAllMines()
+    {
+        foreach (var cell in board.Cast<Cell>().Where(p => p.HasMine))
+        {
+            cell.IsOpened = true;
+        }
+    }
+
+    private void DoActionAroundCell(Cell cell, Action<int, int> action)
+    {
+        int row = cell.Row;
+        int col = cell.Column;
+
+        for (int i = Math.Max(0, row - 1); i <= Math.Min(BoardSize - 1, row + 1); i++)
+        {
+            for (int j = Math.Max(0, col - 1); j <= Math.Min(BoardSize - 1, col + 1); j++)
+            {
+                action.Invoke(i, j);
+            }
+        }
+    }
+}
