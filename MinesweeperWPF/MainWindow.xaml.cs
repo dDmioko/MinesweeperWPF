260074aeaa4e03b3909dc363312038b440f13cd6
dDmioko <aalexn64@gmail.com>
2023-05-28 14:30:54 +0700

    Add timer and checkedMines counters

--- MinesweeperWPF/MainWindow.xaml.cs
+++ MinesweeperWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using MinesweeperWPF.Lib;
 
-using MinesweeperWPF.Lib;
+using System.Windows;
+using System.Windows.Threading;
+
 namespace MinesweeperWPF;
 
 /// <summary>
@@ -11,13 +13,37 @@ public partial class MainWindow : Window
     private const int BoardSize = 10; // Размер игрового поля
     private const int MineCount = 15; // Количество мин
 
+    /// <summary>
+    /// Количество отмеченных мин
+    /// </summary>
+    private int _checkedMines;
+
+    /// <summary>
+    /// Таймер с интервалом в 1 секунду
+    /// </summary>
+    private readonly DispatcherTimer _timer = new()
+    {
+        Interval = TimeSpan.FromSeconds(1),
+    };
+    private DateTime _startTime;
+
+    /// <summary>
+    /// Класс с реализацией игры
+    /// </summary>
     private Game _game = new(BoardSize, MineCount);
 
     public MainWindow()
     {
         InitializeComponent();
-        GenerateBoard();
+        InitializeMainWindow();
+        ResetGame();
     }
+
+    private void InitializeMainWindow()
+    {
+        _timer.Tick += Timer_Tick;
+    }
+
 
     private void GenerateBoard()
     {
@@ -31,13 +57,43 @@ public partial class MainWindow : Window
         }
     }
 
+    private void ResetGame()
+    {
+        BoardGrid.Children.Clear();
+
+        // Очищаем количество отмеченных мин
+        _checkedMines = 0;
+        UpdateCheckedMines();
+
+        // Очищаем таймер
+        _startTime = DateTime.Now;
+        Timer_Tick(null, null);
+
+        _game = new Game(BoardSize, MineCount);
+        GenerateBoard();
+    }
+
+    private void UpdateCheckedMines()
+    {
+        CheckedMinesBlock.Text = $"Количество мин: {_checkedMines}";
+    }
+
     private void Cell_MouseLeftButtonDown(object sender, RoutedEventArgs e)
     {
+        // Если таймер не запущен, то запускаем
+        if (!_timer.IsEnabled)
+        {
+            _startTime = DateTime.Now;
+            _timer.Start();
+        }
+
         var cell = (Cell)sender;
         if (cell.HasMine)
         {
             // Игра проиграна
             _game.ShowAllMines();
+            _timer.Stop();
+
             MessageBox.Show("Вы проиграли!", "Поражение", MessageBoxButton.OK, MessageBoxImage.Exclamation);
             ResetGame();
             return;
@@ -51,10 +107,11 @@ public partial class MainWindow : Window
         ResetGame();
     }
 
-    private void ResetGame()
+
+    private void Timer_Tick(object? sender, EventArgs? e)
     {
-        BoardGrid.Children.Clear();
-        _game = new Game(BoardSize, MineCount);
-        GenerateBoard();
+        // Вычисление прошедшего времени и обновление TimerBlock
+        TimeSpan elapsedTime = DateTime.Now - _startTime;
+        TimerBlock.Text = elapsedTime.ToString(@"hh\:mm\:ss");
     }
 }
